b730f3711a322f246bab2f49e20438e7ccb56955
GurnMi <dlrjsak3@naver.com>
2018-04-18 10:37:56 +0900

    login

--- app/src/main/java/kr/or/dgit/bigdata/todaymanagerapp/MainActivity.java
+++ app/src/main/java/kr/or/dgit/bigdata/todaymanagerapp/MainActivity.java
@@ -1,13 +1,94 @@
 package kr.or.dgit.bigdata.todaymanagerapp;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.AutoText;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.kakao.auth.ErrorCode;
+import com.kakao.auth.ISessionCallback;
+import com.kakao.auth.Session;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.LoginButton;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.LogoutResponseCallback;
+import com.kakao.usermgmt.callback.MeResponseCallback;
+import com.kakao.usermgmt.response.model.UserProfile;
+import com.kakao.util.exception.KakaoException;
+import com.kakao.util.helper.log.Logger;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
+    private Context mContext;
+
+
+
+   // private Button btn_custom_login;
+
+   // private LoginButton btn_kakao_login;
+
+    private com.kakao.usermgmt.LoginButton btnKakao;
+    private SessionCallback callback;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
+        mContext = getApplicationContext();
+
+        callback = new SessionCallback(); //세션콜백을 부르고
+        Session.getCurrentSession().addCallback(callback); //추가
+
+
+        btnKakao = (LoginButton) findViewById(R.id.btn_kakao_login);
+
+        /**카카오톡 로그아웃 요청**/
+        //한번 로그인이 성공하면 세션 정보가 남아있어서 로그인창이 뜨지 않고 바로 onSuccess()메서드를 호출합니다.
+        //테스트 하시기 편하라고 매번 로그아웃 요청을 수행하도록 코드를 넣었습니다 ^^
+       /* UserManagement.requestLogout( new LogoutResponseCallback() {
+            @Override
+            public void onCompleteLogout() {
+                //로그아웃 성공 후 하고싶은 내용 코딩 ~
+            }
+        });*/
+
+
+
+        //해쉬키 가져오기!!
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.e("MY KEY HASH:",
+                        Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
     }
+
+
+
+
+
+
+
 }
